e939c3e4db985940d9f2f58d949481679f8cba45
Henri Lesourd <henri@mukn.io>
2022-08-25 16:35:03 +0200

    Moving to Gerbil (part I)

---
+++ src/llruntime_gerbil.scm
@@ -0,0 +1,23 @@
+; llruntime[Gerbil].scm
+;
+;  Copyright (C) 2022, MUKN
+;
+;  Authors: Henri Lesourd (August 2022)
+;
+;    This is free software: you can redistribute it and/or modify it under
+;    the terms of the Apache 2.0 License or (at your option) any later version.
+;
+
+(import :std/srfi/13)
+
+;; =>
+(defsyntax (=> LS)
+  (let ((L (syntax->list LS)))
+    `(lambda ,(cadr L) . ,(cddr L))))
+
+(defsyntax (while LS)
+  (let ((L (syntax->list LS)))
+    `(do ()
+         ((not ,(cadr L)))
+         .
+         ,(cddr L))))
