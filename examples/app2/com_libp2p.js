2c54d0a9ab60c088163fc458264b074305798123
HenriLesourd <Henri.Lesourd@gmx.de>
2023-03-21 15:06:52 +0100

    Porting to Javascript-in-the-browser (1-2)

--- examples/app2/com_libp2p.js
+++ examples/app2/com_libp2p.js
@@ -1,30 +1,29 @@
-// com_srv0.js
+// com_libp2p.js
 
 // Basic API
-function net_host() {
-  cli_msg(com_conn() ,["enter-host", com_peerid()]);
+function net_host() { // Not necessary (declare the host to the server)
 }
-function net_enter(PR) {
-  cli_msg(com_conn() ,["net-enter", PR.UIDP, com_peerid()]);
+function net_enter(PR) { // Not necessary
 }
-function net_send(MSG) {
-  cli_msg(com_conn() ,["net-send", MSG]);
+function net_send(MSG) { // Post MSG to TO (UID of the recipient process), taken as a libp2p pubsub topic
 }
 var _COM_MSG=[];
-async function com_poll() {
-  var L=await cli_msg(com_conn() ,["poll", com_peerid()]);
-  for (var MSG of L) {
-    _COM_MSG.push(MSG);
-  }
+async function com_poll() { // Not necessary ; each hosted process should subscribe to a topic that is its own UID
+/*
+ *  NOTE: the subscribe handlers should fill _COM_MSG
+ *  =>
+ *  foreach received message MSG {
+ *    _COM_MSG.push(MSG);
+ *  }
+ */
 }
 function com_isPolling() {
-  return true;
+  return false;
 }
-var _COM_POLLP=2000;
 function com_pollPeriod() {
-  return _COM_POLLP=2000;
+  return -1;
 }
-function net_poll() {
+function net_poll() { // Same in all com layer implementations
   var L=_COM_MSG;
   _COM_MSG=[];
   return L;
@@ -32,14 +31,14 @@ function net_poll() {
 
 // Init
 var _CONN,_PEERID;
-function com_conn() {
-  return _CONN;
+function com_conn() { // Unused ; same in all implementations
+  return _CONN;       // in libp2p-based implementation, _CONN is a libp2p peer object (?)
 }
-function com_peerid() {
+function com_peerid() { // Ask libp2p for this (unused outside of com.js as well)
   return _PEERID;
 }
 function com_connect(URL) {
-  _CONN=cli_conn(URL);
-  _PEERID=gen_uid();
+//_CONN=cli_conn(URL); // Create a peer by means of an URL or of a list of URLs
+//_PEERID=gen_uid();   // Generate the peer's UID
   return _CONN;
 }
